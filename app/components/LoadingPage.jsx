a102a0b9e0878a43105b5793ac66f20d4c7a28c3
isaacDiaz7 <isaac.n.diaz7@gmail.com>
2024-02-07 16:58:20 -0800

    Added initial loading page

---
+++ app/components/LoadingPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./LoadingPage.css";
+
+const LoadingPage = () => {
+  return (
+    <body className="loading-body bg-dark">
+      <div className="container position-absolute top-50 start-50 translate-middle">
+        <div className="text text-white d-flex justify-content-center font-monospace">
+          <h3>Loading...</h3>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div className="progress">
+            <div className="track">
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
+
+export default LoadingPage;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
+"use client";
 import NavBar from "./components/NavBar";
+import LoadingPage from "./components/LoadingPage";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3100);
+  }, []);
+
   return (
-    <main className="bg-black">
-      <NavBar />
-    </main>
+    <React.Fragment>
+      {loading === false ? (
+        <main className="bg-black">
+          <NavBar />
+        </main>
+      ) : (
+        <LoadingPage />
+      )}
+    </React.Fragment>
   );
 }
